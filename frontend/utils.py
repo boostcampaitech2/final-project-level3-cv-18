e843d416e0494c03e9ed07231e108a70e8fd2f1e
김승훈_T2042 <tmdgns1139@naver.com>
2021-12-23 09:33:45 +0900

    Seung/frontend - frontend copy and paste (#23)
    * fix frontend/requirements.txt
    
    * remove comment lines
    
    * remove comment lines

--- frontend/utils.py
+++ frontend/utils.py
@@ -1,17 +1,15 @@
-from array import array
 from datetime import datetime
 import io
 import uuid
 import numpy as np
 from PIL import Image
 
-import albumentations
-import albumentations.pytorch
-import torch
 import streamlit as st
 from gcloud import storage
 from oauth2client.service_account import ServiceAccountCredentials
 
+import urllib.request
+import json
 
 
 credentials_dict = {
@@ -24,7 +22,7 @@ credentials_dict = {
 credentials = ServiceAccountCredentials.from_json_keyfile_dict(
     credentials_dict
 )
-    
+
 def send_to_bucket(image_id:uuid, image_bytes:bytes,label:str,date:datetime):
     client = storage.Client(credentials=credentials, project=st.secrets['gcp']['project_id'])
     bucket = client.get_bucket(st.secrets['gcp']['bucket'])
@@ -40,22 +38,25 @@ def bring_from_bucket(image_name:str):
     return bringImage
 
 def get_naver_api(label:str):
-    import urllib.request
-    import json
+
     client_id = st.secrets['gcp']['X-Naver-Client-Id']
     client_secret = st.secrets['gcp']['X-Naver-Client-Secret']
-    # print("client_id",client_id)
+
     query = label
     query = urllib.parse.quote(query)
+
     url = "https://openapi.naver.com/v1/search/shop?query=" + query
+
     request = urllib.request.Request(url)
     request.add_header('X-Naver-Client-Id', client_id)
     request.add_header('X-Naver-Client-Secret', client_secret)
+
     response = urllib.request.urlopen(request)
     response = json.load(response)
+
     if(len(response['items']) == 0):
         return "Not Exist"
-    # print(json.dumps(response['items'][0],indent=2))
+
     return response['items'][0]
 
 
